3506929a138209f39deb541e405fe876023d8199
Connor Tidy <connortidy@gmail.com>
2026-04-02 09:46:07 -0400

    apparently we shouldnt be doing the chat in app

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -54,7 +54,6 @@ const Header = (props) => {
           <MenuItem onClick={() => handleMenuClick("/Profile")}>Profile</MenuItem>
           <MenuItem onClick={() => handleMenuClick("/Date")}>Date</MenuItem>
           <MenuItem onClick={() => handleMenuClick("/DisplayUser")}>UserProfile</MenuItem>
-          <MenuItem onClick={() => handleMenuClick("/matches")}>Matches</MenuItem>
           <MenuItem onClick={() => handleMenuClick("/Search")}>Search</MenuItem>
           <MenuItem onClick={() => handleMenuClick("/Payment")}>Payment</MenuItem>
           <MenuItem>Love</MenuItem>
