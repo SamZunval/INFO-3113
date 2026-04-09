692b89e09fab69e04d9b38dc3851c78d1c366539
Connor Tidy <connortidy@gmail.com>
2026-04-09 09:30:01 -0400

    Search page has pictures too

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -8,6 +8,7 @@ import {
   Typography,
   Divider,
   Grid,
+  CardMedia,
   Fab  
 } from '@mui/material';
 import * as api from "../util/api"
@@ -106,7 +107,18 @@ const ClickUsermarks = async () => {
             </Typography>
 
             <Divider sx={{ my: 2 }} />
-
+            <CardMedia
+              component="img"
+              height="350"
+              image={selectedUser.profileImage || "https://picsum.photos/345/350?random=1"}
+              alt={`${selectedUser.firstName || selectedUser.userName || 'User'}'s profile`}
+              sx={{
+                objectFit: 'cover'
+              }}
+              onError={(e) => {
+                e.target.src = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzQ1IiBoZWlnaHQ9IjM1MCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cmVjdCB3aWR0aD0iMzQ1IiBoZWlnaHQ9IjM1MCIgZmlsbD0iI2Y1ZjVmNSIvPjxyZWN0IHg9IjEwIiB5PSIxMCIgd2lkdGg9IjMyNSIgaGVpZ2h0PSIyNSIgZmlsbD0iIzk5OSIgcng9IjUiLz48dGV4dCB4PSIxNzIiIHk9IjI3IiBmb250LWZhbWlseT0iQXJpYWwiIGZvbnQtc2l6ZT0iMTIiIGZpbGw9IndoaXRlIiB0ZXh0LWFuY2hvcj0ibWlkZGxlIj5ObyBJbWFnZSBBdmFpbGFibGU8L3RleHQ+PC9zdmc+";
+              }}
+            />
             <Grid container spacing={2}>
               <Grid item xs={6}>
                 <Typography variant="subtitle2">Full Name</Typography>
